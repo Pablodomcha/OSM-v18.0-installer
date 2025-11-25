9bb4fa5bba315f4f5a2b2e328d6906254ef682d9
Pablodomcha <pablo.dominguez.chafer@alumnos.upm.es>
2025-11-25 12:52:20 +0100

    improved logging visibility

--- install-osm-client.sh
+++ install-osm-client.sh
@@ -1,13 +1,16 @@
 OSM_CLIENT_VERSION="v18.0"
-echo $OSM_CLIENT_VERSION
+echo "OSM_CLIENT_VERSION=$OSM_CLIENT_VERSION"
 OSM_IM_VERSION="v18.0"
-echo $OSM_IM_VERSION
+echo "OSM_IM_VERSION=$OSM_IM_VERSION"
+echo "------------------------------"
+echo "| Installing DEBIAN FRONTEND |"
+echo "------------------------------"
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y python3 python3-setuptools python3-dev python3-pip
-echo $DEBIAN_FRONTEND
+echo "DEBIAN_FRONTEND=$DEBIAN_FRONTEND"
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y libmagic1
-echo $DEBIAN_FRONTEND
+echo "DEBIAN_FRONTEND=$DEBIAN_FRONTEND"
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y make
-echo $DEBIAN_FRONTEND
+echo "DEBIAN_FRONTEND=$DEBIAN_FRONTEND"
 echo "Running the pip install"
 sudo -H python3 -m pip install -U pip
 # Install OSM IM and its dependencies via pip
@@ -20,7 +23,7 @@ echo "--------------------------------------------------------------------------
 python3 -m pip install -r "https://osm.etsi.org/gitweb/?p=osm/IM.git;a=blob_plain;f=requirements.txt;hb=${OSM_IM_VERSION}"
 # Path needs to include $HOME/.local/bin in order to use pyang
 [ "$(which pyang)" = "$HOME/.local/bin/pyang" ] || export PATH=$HOME/.local/bin:${PATH}
-echo "-----------------------------------------------"
+echo "----------------------------------------------------------------------------------------------"
 echo "Installing git+https://osm.etsi.org/gerrit/osm/IM.git@${OSM_IM_VERSION}#egg=osm-im (step 1/3)"
 echo "----------------------------------------------------------------------------------------------"
 python3 -m pip install "git+https://osm.etsi.org/gerrit/osm/IM.git@${OSM_IM_VERSION}#egg=osm-im" --upgrade --no-build-isolation
