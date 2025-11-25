3163781ffbe9328f164bc91a069cbbfb4331439c
Pablodomcha <pablo.dominguez.chafer@alumnos.upm.es>
2025-11-25 12:43:05 +0100

    made logging messages more visible

--- install-osm-client.sh
+++ install-osm-client.sh
@@ -11,16 +11,24 @@ echo $DEBIAN_FRONTEND
 echo "Running the pip install"
 sudo -H python3 -m pip install -U pip
 # Install OSM IM and its dependencies via pip
-echo "-----------------------------------------------"
-echo "Installing OSM IM and its dependencies via pip"
-echo "-----------------------------------------------"
+echo "--------------------------------------------------"
+echo "| Installing OSM IM and its dependencies via pip |"
+echo "--------------------------------------------------"
+echo "----------------------------------------------------------------------------------------------"
 echo "Installing https://osm.etsi.org/gitweb/?p=osm/IM.git;a=blob_plain;f=requirements.txt;hb=${OSM_IM_VERSION}"
+echo "----------------------------------------------------------------------------------------------"
 python3 -m pip install -r "https://osm.etsi.org/gitweb/?p=osm/IM.git;a=blob_plain;f=requirements.txt;hb=${OSM_IM_VERSION}"
 # Path needs to include $HOME/.local/bin in order to use pyang
 [ "$(which pyang)" = "$HOME/.local/bin/pyang" ] || export PATH=$HOME/.local/bin:${PATH}
+echo "-----------------------------------------------"
 echo "Installing git+https://osm.etsi.org/gerrit/osm/IM.git@${OSM_IM_VERSION}#egg=osm-im (step 1/3)"
+echo "----------------------------------------------------------------------------------------------"
 python3 -m pip install "git+https://osm.etsi.org/gerrit/osm/IM.git@${OSM_IM_VERSION}#egg=osm-im" --upgrade --no-build-isolation
+echo "----------------------------------------------------------------------------------------------"
 echo "Installing https://osm.etsi.org/gitweb/?p=osm/osmclient.git;a=blob_plain;f=requirements.txt;hb=${OSM_CLIENT_VERSION} (step 2/3)"
+echo "----------------------------------------------------------------------------------------------"
 python3 -m pip install -r "https://osm.etsi.org/gitweb/?p=osm/osmclient.git;a=blob_plain;f=requirements.txt;hb=${OSM_CLIENT_VERSION}"
+echo "----------------------------------------------------------------------------------------------"
 echo "Installing git+https://osm.etsi.org/gerrit/osm/osmclient.git@${OSM_CLIENT_VERSION}#egg=osmclient (step 3/3)"
+echo "----------------------------------------------------------------------------------------------"
 python3 -m pip install git+https://osm.etsi.org/gerrit/osm/osmclient.git@${OSM_CLIENT_VERSION}#egg=osmclient
