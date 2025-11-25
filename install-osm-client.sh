4face740e0ffc7892c07e10eaf0b63f7114795ca
Pablodomcha <pablo.dominguez.chafer@alumnos.upm.es>
2025-11-25 12:27:15 +0100

    added the --no-build-isolation to the client installer

--- install-osm-client.sh
+++ install-osm-client.sh
@@ -8,6 +8,6 @@ sudo -H python3 -m pip install -U pip
 python3 -m pip install -r "https://osm.etsi.org/gitweb/?p=osm/IM.git;a=blob_plain;f=requirements.txt;hb=${OSM_IM_VERSION}"
 # Path needs to include $HOME/.local/bin in order to use pyang
 [ "$(which pyang)" = "$HOME/.local/bin/pyang" ] || export PATH=$HOME/.local/bin:${PATH}
-python3 -m pip install "git+https://osm.etsi.org/gerrit/osm/IM.git@${OSM_IM_VERSION}#egg=osm-im" --upgrade
+python3 -m pip install "git+https://osm.etsi.org/gerrit/osm/IM.git@${OSM_IM_VERSION}#egg=osm-im" --upgrade --no-build-isolation
 python3 -m pip install -r "https://osm.etsi.org/gitweb/?p=osm/osmclient.git;a=blob_plain;f=requirements.txt;hb=${OSM_CLIENT_VERSION}"
 python3 -m pip install git+https://osm.etsi.org/gerrit/osm/osmclient.git@${OSM_CLIENT_VERSION}#egg=osmclient
