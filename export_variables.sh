644fc7477928514ebdbf6987566bc8021a397375
Pablodomcha <pablo.dominguez.chafer@alumnos.upm.es>
2026-02-05 13:53:28 +0100

    removed edit IP file and added it's functionality to export variables

--- export_variables.sh
+++ export_variables.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# 1. Update the local kubeconfig first
-./edit_IP.sh
-
-# 2. Get the actual current IP of the interface
+# 1. Get the actual current IP of the interface
 CURRENT_IP=$(ip -4 addr show enp0s3 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+
+# 2. Update the local kubeconfig first
+sed -i "s|server: https://.*:6443|server: https://${CURRENT_IP}:6443|" ~/.kube/config
 
 # 3. Force update the Ingresses in K8s to use the NEW IP
 kubectl patch ingress nbi-ingress -n osm --type='json' -p="[{\"op\": \"replace\", \"path\": \"/spec/rules/0/host\", \"value\":\"nbi.${CURRENT_IP}.nip.io\"}]" > /dev/null
