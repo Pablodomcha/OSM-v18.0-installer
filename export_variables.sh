a166bad5f68bb3663e339c72204a1f399b29cdfd
Pablodomcha <pablo.dominguez.chafer@alumnos.upm.es>
2026-02-05 13:50:44 +0100

    edited script to auto update the IP in OSM too

--- export_variables.sh
+++ export_variables.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-# Edit the file to use the new IP
+# 1. Update the local kubeconfig first
 ./edit_IP.sh
 
-# Save the values of the variables needed and echo them
+# 2. Get the actual current IP of the interface
+CURRENT_IP=$(ip -4 addr show enp0s3 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+
+# 3. Force update the Ingresses in K8s to use the NEW IP
+kubectl patch ingress nbi-ingress -n osm --type='json' -p="[{\"op\": \"replace\", \"path\": \"/spec/rules/0/host\", \"value\":\"nbi.${CURRENT_IP}.nip.io\"}]" > /dev/null
+kubectl patch ingress ngui-ingress -n osm --type='json' -p="[{\"op\": \"replace\", \"path\": \"/spec/rules/0/host\", \"value\":\"gui.${CURRENT_IP}.nip.io\"}]" > /dev/null
+
+# 4. Now capture the variables as you did before
 export OSM_HOSTNAME=$(kubectl get -n osm -o jsonpath="{.spec.rules[0].host}" ingress nbi-ingress)
 echo "OSM_HOSTNAME (for osm client): $OSM_HOSTNAME"
+
 export OSM_GUI_URL=$(kubectl get -n osm -o jsonpath="{.spec.rules[0].host}" ingress ngui-ingress)
 echo "OSM UI: $OSM_GUI_URL"
