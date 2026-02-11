0be5514c0660dd2cd05854de3e4a4558ee2a2550
Pablodomcha <pablo.dominguez.chafer@alumnos.upm.es>
2026-02-11 15:01:44 +0100

    edited osm command to install with LXD for juju

--- install-LXD.sh
+++ install-LXD.sh
@@ -6,3 +6,7 @@ sudo apt-get install zfsutils-linux -y
 sudo snap install lxd
 
 sudo cp LXD-files/60-lxd-production.conf /etc/sysctl.d/60-lxd-production.conf
+sudo sysctl --system
+
+lxc network create lxdbr0 ipv6.address=none ipv4.address=10.0.2.15/24 ipv4.nat=true
+lxd init
