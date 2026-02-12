c0623a0f4f77c7f92cc08c676b42c66ec1919e75
Pablodomcha <pablo.dominguez.chafer@alumnos.upm.es>
2026-02-12 11:49:40 +0100

    removed the lxd install option to the command

--- install-osm-18.sh
+++ install-osm-18.sh
@@ -8,7 +8,7 @@ touch "$MARKER_FILE"
 sudo apt install net-tools
 wget https://osm-download.etsi.org/ftp/osm-18.0-eighteen/install_osm.sh
 chmod +x install_osm.sh
-./install_osm.sh -y --juju --lxd
+./install_osm.sh -y
 
 # Change the necessary files for the installation to work
 sudo cp osm-install-files/10-install-client-tools.sh /usr/share/osm-devops/installers/10-install-client-tools.sh
@@ -17,7 +17,7 @@ sudo rm -r /usr/share/osm-devops/installers/helm/osm/charts
 sudo cp -r osm-install-files/charts /usr/share/osm-devops/installers/helm/osm/charts
 
 # Run the install again, saving a log of the installation, in case it's needed for troubleshooting
-./install_osm.sh --juju --lxd -y 2>&1 | tee osm_install_log.txt
+./install_osm.sh -y 2>&1 | tee osm_install_log.txt
 
 # Save the values of the variables needed and echo them
 export OSM_HOSTNAME=$(kubectl get -n osm -o jsonpath="{.spec.rules[0].host}" ingress nbi-ingress)
