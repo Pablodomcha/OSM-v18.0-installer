2f1f2355d02eff30f2860b88a715940105539224
Pablodomcha <pablo.dominguez.chafer@alumnos.upm.es>
2026-02-12 12:39:03 +0100

    removed uneeded files

--- Test-Commands/lcm_ping.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-kubectl -n osm exec -it lcm-65474b445d-shffn -- python3 -c "import socket; s = socket.socket(); s.settimeout(5); print('CONNECTED' if s.connect_ex(('10.0.2.6', 16443)) == 0 else 'FAILED')"
